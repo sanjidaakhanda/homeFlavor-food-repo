cd70eba33e61b51474902a554ce256da9e4e9f72
Your Name <sanjida.ier@gmail.com>
2022-11-08 16:51:41 -0500

    menucard  added commit

---
+++ src/Pages/Home/Menus/MenuCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const MenuCard = ({ menu }) => {
+  const { image, name, price } = menu;
+  return (
+    <div>
+      <div className="card card-compact  bg-base-100 shadow-xl">
+        <figure>
+          <img src={image} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-rose-500">{name}</h2>
+          <p className="text-black-500 font-semibold text-xl">
+            {" "}
+            Price: {price}
+          </p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-error">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MenuCard;
--- src/Pages/Home/Menus/Menus.js
+++ src/Pages/Home/Menus/Menus.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import MenuCard from "./MenuCard";
 
 const Menus = () => {
+  const [menus, setMenus] = useState([]);
+
+  useEffect(() => {
+    fetch("Menus.json")
+      .then((res) => res.json())
+      .then((data) => setMenus(data));
+  }, []);
+
   return (
     <div>
       <p className="text-4xl text-rose-500 text-center mt-5 font-semibold">
         Our Menus
       </p>
-      <p className=" font-semibold text-center">
+      <p className=" font-semibold text-center mb-5">
         Unfussy halal eatery specializing in fried chicken, plus subs,
         gyros,seafood, burgers <br /> & salads. pick your favorite item from
         here.
       </p>
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {menus.map((menu) => (
+          <MenuCard key={menu.id} menu={menu}></MenuCard>
+        ))}
+      </div>
     </div>
   );
 };
