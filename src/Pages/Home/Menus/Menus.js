762bf25fac1d755193f361c2652b15a6bbcb0cac
Your Name <sanjida.ier@gmail.com>
2022-11-08 15:29:20 -0500

    banner  added commit

--- src/Pages/Home/Menus/Menus.js
+++ src/Pages/Home/Menus/Menus.js
@@ -6,9 +6,10 @@ const Menus = () => {
       <p className="text-4xl text-rose-500 text-center mt-5 font-semibold">
         Our Menus
       </p>
-      <p className=" font-semibold">
+      <p className=" font-semibold text-center">
         Unfussy halal eatery specializing in fried chicken, plus subs,
-        gyros,seafood, burgers & salads. pick your favorite item from here.
+        gyros,seafood, burgers <br /> & salads. pick your favorite item from
+        here.
       </p>
     </div>
   );
